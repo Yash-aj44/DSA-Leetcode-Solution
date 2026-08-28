f8ad7889a6e99e5023c796734e0cecbf47604d43
Yashwanth AJ <srivatsan2k6@gmail.com>
2026-08-28 12:51:31 +0530

    Time: 7 ms (5.31%) | Memory: 10.7 MB (7.89%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        string temp = "";
+
+        for(char c : s) {
+
+            if(isalnum(c)) {
+                temp += tolower(c);
+            }
+        }
+
+        string rev = temp;
+        reverse(rev.begin(), rev.end());
+
+        return temp == rev;
+    }
+};
